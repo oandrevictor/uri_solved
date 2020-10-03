aea4fc0c341f2abda239e3920b1327a7b820b4c8
André Andrade <oandrevictor@gmail.com>
2020-10-03 14:27:43 -0300

    Create 1624.py
    Solution for https://www.urionlinejudge.com.br/judge/pt/runs/code/7565325 in Python 2.7 (Python 2.7.6) [+1s]
    
    TEMPO:
    0.012s
    TAMANHO:
    593 Bytes

---
+++ 1624.py
@@ -0,0 +1,25 @@
+	n = int(raw_input())
+	if n==0:
+		break
+	pesos = []
+	precos = []
+	for i in range(n):  
+		preco, peso = map(int,raw_input().split())
+		pesos.append(peso)
+		precos.append(preco)
+	m= int(raw_input())
+	dp = [[0 for x in range(m+1)] for y in range(n+1)] 
+	dp[0][0] = 0		
+	
+	for i in range(n+1):
+		for p in range(m+1):
+			if i==0 or p ==0:
+				dp[i][p] = 0
+			else:
+				if pesos[i-1] > p:
+					dp[i][p] = dp[i-1][p]
+				else:
+					dp[i][p] = max(dp[i-1][p], dp[i-1][p-pesos[i-1]] + precos[i-1])
+				
+
+	print dp[n][m]
